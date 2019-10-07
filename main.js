806dc04beca271f346f4c1cfe1820da05d0a54af
Jimmy Lin <jimmydlin@gmail.com>
2019-10-07 16:43:57 -0700

    js game v2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var hungry = confirm("Do you want to play the lunch?")
+var hungry = confirm("Do you want to go to lunch?")
 
 
 if (hungry) {
@@ -57,26 +57,43 @@ function chinese() {
 }
 
 function indian(){
-    var spice = prompt("Do you like your food spicy or mild?")
-    if (spice.toLowerCase() == "spicy") {
-        alert("Let's go to Taj Palace")
+    var hunger = prompt("What is your hunger level between 1 and 10?")
+    if (hunger > 6 && hunger < 11) {
+        alert("Wow, you're hungry..let's go to Taj Buffet")
     }
-    else if (spice.toLowerCase() == "mild") {
-        alert("Let's go to Curry House")
+    else if (hunger > 3 && hunger < 7) {
+        alert("Let's go to Curry House, they have a la carte")
+    }
+    else if (hunger > 0 && hunger < 4) {
+        alert("Go eat a salad you vegan!!")
     }
     else {
-        alert("Please learn to type...try again next week!")
+        alert("I didn't understand, I'll just pick for us")
     }
 
 }
 
 function mexican(){
-    var spice = prompt("Do you like your food spicy or mild?")
-    if (spice.toLowerCase() == "spicy") {
-        alert("Let's go to Roberto's")
+    
+    var location = prompt("Do you want to go to Henderson or Summerlin?")
+    if (location.toLowerCase() == "henderson") {
+        alert("Juan's Flaming Fajitas is great!")
+        var yesnomexican = confirm ("Do you want to go there?")
+        if (yesnomexican) {
+            alert("Cool, I'll drive")
+        } 
+        else {
+            alert("No?? Fine, I'll go by myself")
+        }
     }
-    else if (spice.toLowerCase() == "mild") {
-        alert("Let's go to Taco Bell")
+    else if (location.toLowerCase() == "summerlin") {
+        var drive = confirm("Can you drive?")
+        if (drive) {
+            alert("Cool, let's go to Red Rock")
+        }
+        else {
+            alert("Fine, let's just go to taco bell down the street")
+        }
     }
     else {
         alert("Please learn to type...try again next week!")
@@ -94,6 +111,7 @@ function teatype(){
         else {
             alert("I didn't understand, I'll order you water")
         }
+
 
 
 }
